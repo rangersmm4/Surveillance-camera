1c98eb6be384c5a2dae9e937c18d1d791b42016f
rangersmm4 <mimiwu8954@gmail.com>
2024-05-14 14:22:51 +0800

    Create motion-slack.sh

---
+++ motion-slack.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+LOG_FILE='/var/log/motion/motion.log'
+WEBHOOK_URL='https://hooks.slack.com/services/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'
+USERNAME='motion'
+CHANNEL='#motion-detect'
+
+function send_slack_message {
+  DATA="payload={\"username\": \"${USERNAME}\", \"channel\": \"${CHANNEL}\", \"text\": \"$1\"}"
+  echo "data: ${DATA}"
+  curl -X POST --data-urlencode "${DATA}" "${WEBHOOK_URL}";
+  echo ""
+}
+
+send_slack_message 'motion alert started!'
+
+tail -n0 -F "${LOG_FILE}" | while read LINE; do
+  if [[ $LINE == *"event_newfile"* ]]; then
+    send_slack_message "motion detected!"
+  fi
+
+done
